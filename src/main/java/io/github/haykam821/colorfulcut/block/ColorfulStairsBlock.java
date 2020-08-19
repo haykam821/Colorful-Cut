0290d99dc1f43258d3681db73185be852063f80b
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-08-19 08:28:46 -0400

    Fix double concrete and terracotta slabs only dropping one slab

--- src/main/java/io/github/haykam821/colorfulcut/block/ColorfulStairsBlock.java
+++ src/main/java/io/github/haykam821/colorfulcut/block/ColorfulStairsBlock.java
@@ -2,7 +2,6 @@ package io.github.haykam821.colorfulcut.block;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.StairsBlock;
 
 public class ColorfulStairsBlock extends StairsBlock {
